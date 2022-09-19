3e44fa2df50ef48417145678dfa4213544633e4f
Raphael Darley <raphael@raphaeldarley.com>
2022-09-19 17:38:04 +0000

    after episode 2

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,38 @@
 use rand::{self, Rng};
-use std::{error::Error, fmt};
+use std::{collections::BTreeMap, error::Error, fmt, sync::Arc, thread};
 use surrealdb::{
+    sql::{self, Data},
     sql::{Thing, Value},
     Datastore, Session,
 };
 use tokio;
+use tokio::sync::Mutex;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // let ds = Datastore::new("memory").await?;
-    let ds = Datastore::new("file://test.db").await?;
+    let ds = Datastore::new("memory").await?;
+    // let ds = Datastore::new("file://test.db").await?;
 
     let ses = Session::for_kv().with_ns("test").with_db("test");
     // let ast = r#"CREATE entry SET when = time::now(), num = {};"#;
 
-    let entry_num = 10_000;
-    // for i in 0..entry_num {
-    //     ds.execute(
-    //         &format!(r#"CREATE entry SET when = time::now(), num = {};"#, i),
-    //         &ses,
-    //         None,
-    //         false,
-    //     )
-    //     .await?;
-    // }
-
-    // let select_response = ds
-    //     .execute("SELECT * FROM entry;", &ses, None, false)
-    //     .await?;
-    // // println!("{:#?}", select_response);
+    let entry_num = 2;
+    for i in 0..entry_num {
+        let mut vars = BTreeMap::new();
+        vars.insert(
+            "num".to_string(),
+            sql::Value::Number(sql::Number::Int(i as i64)),
+        );
+        ds.execute(
+            r#"CREATE entry SET when = time::now(), num = $num;"#,
+            &ses,
+            Some(vars),
+            false,
+        )
+        .await?;
+    }
 
     // let select_res = select_response[0].output().unwrap();
-
-    // let mut id_acc = vec![];
 
     // if let Value::Array(rows) = select_res {
     //     for row in rows.iter() {
@@ -47,27 +47,71 @@ async fn main() -> Result<(), Box<dyn Error>> {
     //     println!("{:?}", id.to_string());
     // }
 
-    let mut rng = rand::thread_rng();
-
-    // let select_response = ds
-    //     .execute(
-    //         &format!(
-    //             "SELECT * FROM entry WHERE num = {};",
-    //             rng.gen_range(0..entry_num)
-    //         ),
-    //         &ses,
-    //         None,
-    //         false,
-    //     )
-    //     .await?;
-
     let select_response = &ds
         .execute("SELECT * FROM entry;", &ses, None, false)
         .await?;
 
-    let select_res = select_response[0].output().unwrap();
+    let select_result = select_response[0].output().unwrap();
 
-    if let Value::Array(rows) = select_res {
+    let mut id_acc = vec![];
+
+    if let Value::Array(rows) = select_result {
+        for row in rows.iter() {
+            if let Value::Object(obj) = row {
+                // println!("{:?}", obj)
+                id_acc.push(obj.rid().unwrap());
+            }
+        }
+    }
+
+    println!("time taken:{}", select_response[0].speed());
+
+    // for id in id_acc {
+    //     println!("{:?}", id.to_string());
+    // }
+
+    // let mut vars2 = BTreeMap::new();
+    // vars2.insert("rid".to_string(), sql::Value::Thing(id_acc[0].clone()));
+    // let select_response2 = &ds
+    //     .execute("SELECT * FROM $rid;", &ses, Some(vars2), false)
+    //     .await?;
+
+    // let select_result2 = select_response2[0].output().unwrap();
+
+    // if let Value::Array(rows) = select_result2 {
+    //     for row in rows.iter() {
+    //         if let Value::Object(obj) = row {
+    //             println!("{:?}", obj)
+    //         }
+    //     }
+    // }
+
+    let thread_ds = Arc::new(Mutex::new(ds));
+    let thread_ses = Arc::new(Mutex::new(ses));
+    let db_thread = {
+        let thread_ds_clone = thread_ds.clone();
+        let thread_ses_clone = thread_ses.clone();
+        thread::spawn(|| async move { print_db_entries(thread_ds_clone, thread_ses_clone).await })
+    };
+
+    let test = db_thread.join().unwrap().await;
+
+    print_db_entries(thread_ds, thread_ses).await.unwrap();
+
+    Ok(())
+}
+
+async fn print_db_entries(
+    thread_ds: Arc<Mutex<Datastore>>,
+    thread_ses: Arc<Mutex<Session>>,
+) -> Result<(), Box<dyn Error>> {
+    let ds = &*thread_ds.lock().await;
+    let ses = &*thread_ses.lock().await;
+    let select_response = ds.execute("SELECT * FROM entry;", ses, None, false).await?;
+
+    let select_result = select_response[0].output().unwrap();
+
+    if let Value::Array(rows) = select_result {
         for row in rows.iter() {
             if let Value::Object(obj) = row {
                 println!("{:?}", obj)
@@ -75,38 +119,5 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    println!("time taken:{}", select_response[0].speed());
     Ok(())
-}
-
-fn get_thing(val: &Value) -> Result<&Thing, GenericError> {
-    match val {
-        Value::Thing(t) => Ok(t),
-        _ => Err(GenericError::new("No Thing in Value enum")),
-    }
-}
-
-#[derive(Debug)]
-struct GenericError {
-    details: String,
-}
-
-impl GenericError {
-    fn new(msg: &str) -> Self {
-        Self {
-            details: msg.to_string(),
-        }
-    }
-}
-
-impl fmt::Display for GenericError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.details)
-    }
-}
-
-impl Error for GenericError {
-    fn description(&self) -> &str {
-        &self.details
-    }
 }
